4f8e8dd96ffa9a6e29a988101b381948a8608dcf
Florian H-J <fhj+km@fruitice.fr>
2017-07-11 11:23:48 +0200

    Beginning of life

---
+++ index.js
@@ -0,0 +1,14 @@
+const spiderlink = require('spiderlink')('envision')
+const pmx = require('pmx')
+
+pmx.init({
+  isModule: true
+})
+
+class Envision {
+  constructor() {
+
+  }
+}
+
+module.exports = Envision
