1bcc79e82d4f50cb96f1654f65d7917957276a59
Florian H-J <florian@keymetrics.io>
2018-01-03 12:09:23 +0100

    (socket) add unix socket connector

--- index.js
+++ index.js
@@ -84,39 +84,60 @@ class EnvisionModule {
     if (typeof (this.onStop) !== 'function') throw new Error('onStop must handled')
 
     this.name = pmx._pmx_conf.module_name
+
+    this.connectEnvision()
+  }
+
+  connectEnvision () {
     console.log(`Connecting to ws+unix://${process.env.HOME}/envision`)
     this.ws = new WebSocket(`ws+unix://${process.env.HOME}/envision`)
     this.json = (obj) => this.ws.send(JSON.stringify(obj))
 
     this.ws.on('open', () => {
+      console.log('UnixWS opened')
       this.json({ action: 'getModuleInfos' })
+    })
+
+    this.ws.on('error', (err) => {
+      console.log(err)
+    })
+
+    this.ws.on('close', () => {
+      console.log('close')
+      this.onStop(() => {
+        if (this.server) this.server.close()
+        setTimeout(this.connectEnvision.bind(this), 1000)
+      })
     })
 
     this.ws.on('message', msg => {
       try {
         msg = JSON.parse(msg)
+        console.log(msg)
       } catch (error) {
         return
       }
+      let data = msg.data
 
       switch (msg.action) {
         case 'sendModuleInfos':
-          if (module.err) throw new Error(module.err)
+          if (data.err) throw new Error(data.err)
 
-          this.port = module.port
+          this.port = data.port
 
           this.onStart(app, () => {
             if (this.onLocal) app.use('/screen', this.onLocal)
             if (this.onRemote) app.use('/config', this.onRemote)
 
-            app.listen(this.port, () => {
+            this.server = app.listen(this.port, () => {
               console.log('Module listening on port ' + this.port)
               if (typeof (this.onStarted) === 'function') this.onStarted(this.port)
+              this.ws.json({ action: 'online' })
             })
           })
           break
         default:
-          if (this.stack[msg.action]) {
+          if (typeof this.stack[msg.action] === 'function') {
             this.stack[msg.action](msg.data)
           }
           break
